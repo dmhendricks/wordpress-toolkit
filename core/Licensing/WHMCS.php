dd52f2dde6a3b4127748c658ae43bf0857ad2e9d
Daniel Hendricks <mail@danhendricks.com>
2017-12-30 15:23:54 -0600

    Added skeleton WHMCS_License class

---
+++ core/Licensing/WHMCS.php
@@ -0,0 +1,39 @@
+<?php
+namespace WordPress_ToolKit\Licensing;
+use WordPress_ToolKit\ConfigRegistry;
+use WordPress_ToolKit\ToolKit;
+
+/**
+  * A class to perform license code verification via the WHMCS Licensing Addon.
+  *
+  * @link https://www.whmcs.com/software-licensing/
+  * @see https://docs.whmcs.com/Licensing_Addon
+  * @since 0.2.0
+  */
+class WHMCS_License extends ToolKit
+{
+
+  protected $whmcs_url;
+  protected $licensing_secret_key;
+  protected $local_key_expire_days;
+  protected $allow_check_fail_days;
+
+  /**
+   * Validate user-provided license key
+   *
+   * @param string $key License key to validate
+   * @param string $args Licensed product settings (overrides values
+   *    passed by base class; required if not passed by base class)
+   * @return array Result from validation attempt
+   * @since 0.2.0
+   */
+  public function validate( $key, $args = array() )
+  {
+
+    $result = array( 'valid': false, 'message': 'Invalid or missing server/product settings.' );
+
+    return $result;
+
+  }
+
+}
